c453cea44d49ba5f7d0e5b33a3028d8a1381037f
UnaiBermejo <unaibermejofdez@opendeusto.es>
2020-10-08 10:16:45 +0000

    Minor change

--- new-experiments/activity_change_recognition/supervised/LSTM/activity_change_recognition_lstm.py
+++ new-experiments/activity_change_recognition/supervised/LSTM/activity_change_recognition_lstm.py
@@ -158,10 +158,10 @@ def main(argv):
     # check actions input shape
     print("Input action shape: " + str(X_actions.shape))
     # repeat exe iterations
-    results_1 = np.zeros((4,10,30))
-    results_5 = np.zeros((4,10,30))
-    results_10 = np.zeros((4,10,30))
-    detection_delays = np.zeros((10,30))
+    results_1 = np.zeros((4,10,exe))
+    results_5 = np.zeros((4,10,exe))
+    results_10 = np.zeros((4,10,exe))
+    detection_delays = np.zeros((10,exe))
     models = []
     for e in range(0, exe):
         # input pipeline
